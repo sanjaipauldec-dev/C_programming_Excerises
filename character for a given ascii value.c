d832d5a516e7007c39e7f3c921b078c015409356
sanjaipauldec-dev <sanjaipauldec@gmail.com>
2025-09-04 14:58:57 +0530

    Add files via upload

---
+++ character for a given ascii value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("ascii value of a character");
+	scanf("%c",&num);
+	printf("%d",num);
+	return 0;
+	
+
+}
---
+++ character in single quotes.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("character in a single quote");
+	scanf("%d",&a);
+	printf("%d""/""",a);
+	return 0;
+}
